246fc7ba393e8d42b8260efd34280c0734715c93
Andrew-Bogolubof <a.bogolubof@gmail.com>
2019-02-20 17:59:29 +0300

    Love-triangle implementation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+
+    for (let i = 0; i < preferences.length; i++) {
+        let first = preferences[i];
+        for (let j = i + 1; j < preferences.length; j++) {
+            let second = preferences[j];
+            for (let k = i + 1; k < preferences.length; k++) {
+                let third = preferences[k];
+
+                if ( (i + 1) == third && (j + 1) == first && (k + 1) == second ) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
 };
